9d232c8a0896b15960acc3d04b27eb81d96f0fe4
Sneha-Rokade <bioblister@gmail.com>
2026-06-02 10:45:50 +0530

    get rest api

---
+++ Sneha_Rokade/rest_api_project/api_app/views.py
@@ -0,0 +1,15 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . models import Student
+from .serializers import StudentSerializer
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request) : 
+    return HttpResponse("Welcome")
+
+@api_view(['GET'])
+def student_list(request):
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
