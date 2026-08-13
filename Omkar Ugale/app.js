c54dcec1a02ecee7a6896a899e8e1faaf708bc28
Omkar Ugale <omkarugale1811@gmail.com>
2026-08-13 15:13:37 +0530

    simp,e react si

---
+++ Omkar Ugale/app.js
@@ -0,0 +1,7 @@
+export const name = "Omkar";
+export let arrr=[20,30,40,60];
+
+
+let a=454;
+
+export default a;
---
+++ Omkar Ugale/style.js
@@ -0,0 +1,10 @@
+
+import { arrr } from "./app.js";
+import { name } from "./app.js";
+import omkar from "./app.js";
+
+
+console.log(name);
+console.log(omkar);
+console.log(arrr);
+
